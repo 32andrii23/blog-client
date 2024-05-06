12db682907b634071d85a5fce6b4ceca55477db3
Andrii Popov <whatyalookinat23@gmail.com>
2024-05-06 20:57:11 +0300

    Add: request interceptor

--- src/http/index.ts
+++ src/http/index.ts
@@ -2,7 +2,14 @@ import axios from "axios";
 
 export const BASE_URL = "https://localhost:3000";
 
-export const httpClient = axios.create({
+const httpClient = axios.create({
     withCredentials: true,
     baseURL: BASE_URL,
 });
+
+httpClient.interceptors.request.use(config => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`
+    return config;
+})
+
+export default httpClient;
